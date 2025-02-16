e610ff344db09d4b8b85e6ff6dc42e13c50599f7
ntdung6868 <ntdungqlqb@gmail.com>
2025-02-16 12:20:34 +0700

    Add Reviews

--- src/main.js
+++ src/main.js
@@ -3,3 +3,21 @@ document.getElementById("play-btn").addEventListener("click", function () {
     document.getElementById("video-thumbnail").classList.add("hidden");
     document.getElementById("video-player").classList.remove("hidden");
 });
+
+var swiper = new Swiper(".review-carousel", {
+    loop: true,
+    spaceBetween: 20,
+    slidesPerView: "auto",
+    pagination: {
+        el: ".reviews-carousel-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+        },
+        992: {
+            slidesPerView: 3,
+        },
+    },
+});
